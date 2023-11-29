a771fd10338a9d76f2f225d707b26b8692196b2f
quocta-uranus <taquoc196@gmail.com>
2023-11-29 09:16:05 +0700

    Update code 28/11/2023

--- app.js
+++ app.js
@@ -1,16 +1,51 @@
-const app ={
-    cars: [],
 
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+
+const app = (() =>{
+    const cars  = ['BMW']
+    const root = $('#root')
+    const input = $('#input')
+    const submit = $('#submit')
+    return {
         add(car) {
-            this.cars.push(car);
-        },
-        edit(index, car) {
-            this.cars[index] = car;
+            cars.push(car)
         },
         detele(index) {
-            this.cars.splice(index, 1);
+            cars.splice(index, 1)
+        },
+        render() {
+            const html = cars.map((car,index) => `<li>
+            ${car}
+            <span class="detele" data-index="${index}" >&times</span>
+            </li>`)
+          .join('')
+            root.innerHTML = html;
+        },
+        handleDelete(e) {
+           const deteleBtn = e.target.closest('.detele');
+        if (deteleBtn) {
+            const index = deteleBtn.dataset.index
+            this.detele(index)
+            this.render()
         }
+        },
+        init() {
+        
+            // Handle DOM events
+            submit.onclick = () => {
+                const car = input.value
+                this.add(car)
+                this.render()
+                input.value = null
+                input.focus()
+            }
+            root.onclick = this.handleDelete.bind(this)
+            this.render()
+        }
+    }
+})();
+app.init()
 
-    
-}
 
