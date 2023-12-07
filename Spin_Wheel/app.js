e58af42c56ec45dc0fa4c0a8614586c1ae730291
quocta-uranus <taquoc196@gmail.com>
2023-12-07 09:33:38 +0700

    complete code

--- Spin_Wheel/app.js
+++ Spin_Wheel/app.js
@@ -160,7 +160,7 @@ function redrawChart() {
         spinSound.play();
 
 function showWinnerAnimation() {
-    // Hiển thị người chiến thắng nếu chỉ còn một người
+    
         if (data.length === 1) {
                         var winner = data[0].name;
             var winnerElement = document.getElementById('winnerMessage');
@@ -225,90 +225,7 @@ var padding = {top:20, right:40, bottom:0, left:0},
               
 
         ];
-        var svg = d3.select('#chart')
-            .append("svg")
-            .data([data])
-            .attr("width",  w + padding.left + padding.right)
-            .attr("height", h + padding.top + padding.bottom);
-        var container = svg.append("g")
-            .attr("class", "chartholder")
-            .attr("transform", "translate(" + (w/2 + padding.left) + "," + (h/2 + padding.top) + ")");
-        var vis = container
-            .append("g");
-            
-        var pie = d3.layout.pie().sort(null).value(function(d){return 1;});
-        // declare an arc generator function
-        var arc = d3.svg.arc().outerRadius(r);
-        // select paths, use arc generator to draw
-        var arcs = vis.selectAll("g.slice")
-            .data(pie)
-            .enter()
-            .append("g")
-            .attr("class", "slice");
-            
-        arcs.append("path")
-            .attr("fill", function(d, i){ return color(i); })
-            .attr("d", function (d) { return arc(d); });
-        // add the text
-        arcs.append("text").attr("transform", function(d){
-                d.innerRadius = 0;
-                d.outerRadius = r;
-                d.angle = (d.startAngle + d.endAngle)/2;
-                return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")translate(" + (d.outerRadius -10) +")";
-            })
-            .attr("text-anchor", "end")
-            .text( function(d, i) {
-                return data[i].name;
-            });
-        container.on("click", spin);
-        function spin(d){
-            
-            container.on("click", null);
-            //all slices have been seen, all done
-            console.log("OldPick: " + oldpick.length, "Data length: " + data.length);
-            if(oldpick.length == data.length){
-                console.log("done");
-                container.on("click", null);
-                return;
-            }
-            var  ps       = 360/data.length,
-                 pieslice = Math.round(1440/data.length),
-                 rng      = Math.floor((Math.random() * 1440) + 360);
-                
-            rotation = (Math.round(rng / ps) * ps);
-            
-            picked = Math.round(data.length - (rotation % 360)/ps);
-            picked = picked >= data.length ? (picked % data.length) : picked;
-            if(oldpick.indexOf(picked) !== -1){
-                d3.select(this).call(spin);
-                return;
-            } else {
-                oldpick.push(picked);
-            }
-            rotation += 90 - Math.round(ps/2);
-            vis.transition()
-                .duration(3000)
-                .attrTween("transform", rotTween)
-                .each("end", function(){
-                    //mark question as seen
-                    d3.select(".slice:nth-child(" + (picked + 1) + ") path")
-                        .attr("fill", "#111");
-                    //populate question
-                    d3.select("#question h1")
-                        .text(data[picked].question);
-                    oldrotation = rotation;
-              
-                    /* Get the result value from object "data" */
-                    console.log(data[picked].value)
-              
-                    /* Comment the below line for restrict spin to sngle time */
-                    container.on("click", spin);
-                });
-            
-
-        }
-
-      
+           
         //make arrow
         
         
@@ -320,17 +237,17 @@ var padding = {top:20, right:40, bottom:0, left:0},
         }
         
        $('.container').on('mouseover', function(){
-  $('.bauble').addClass('light');
-  $('.star').addClass('star-light');
+            $('.bauble').addClass('light');
+            $('.star').addClass('star-light');
 })
 
-$('.container').on('mouseout', function(){
-  $('.bauble').removeClass('light');
+    $('.container').on('mouseout', function(){
+    $('.bauble').removeClass('light');
 }) 
       
-window.addEventListener('load', function() {
-  var audio = document.getElementById('backgroundMusic');
-  audio.play();
+    window.addEventListener('load', function() {
+    var audio = document.getElementById('backgroundMusic');
+        audio.play();
 });
 
 
